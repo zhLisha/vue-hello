40c8ea9c311eac238d3649b028e15859df3a508c
zhLisha <lisazhou1709@gmail.com>
2022-06-23 13:22:54 +0200

    first commit

---
+++ BONUS/script.js
@@ -0,0 +1,10 @@
+var app = new Vue(
+    {
+        el:'#root',
+        data: {
+            titleText: 'Hello!',
+            image: 'https://media-assets.wired.it/photos/615f078b4c30266a0d6379dd/master/w_960,c_limit/1481626523_scimmie-em.jpg',
+            sizeImage: 'image-size'
+        }
+    }
+)
---
+++ script.js
@@ -0,0 +1,8 @@
+var app = new Vue(
+    {
+        el:'#root',
+        data: {
+            titleText: 'Hello!',
+        }
+    }
+)
